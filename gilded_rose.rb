7c433a68cec8f27b925cceed4097b9a001a33501
Kay Lovelace <kay@makersacademy.com>
2017-03-08 14:41:36 +0000

    Rename infinity to domain specific language

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -86,25 +86,25 @@ class FallbackItemProcessor
 end
 
 class GildedRose
-  INF = Float::INFINITY
-  NEGINF = -(Float::INFINITY)
+  MAX_DATE = Float::INFINITY
+  MIN_DATE = -(Float::INFINITY)
   ITEM_PROCESSORS = [
     SpecificItemProcessor.new("Aged Brie", NormalExpiryStrategy.new, QualityStrategy.new(0, 50, {
-      (NEGINF..-1) => Delta.new(2),
-      (0..INF) => Delta.new(1)
+      (MIN_DATE..-1) => Delta.new(2),
+      (0..MAX_DATE) => Delta.new(1)
     })),
     SpecificItemProcessor.new("Backstage passes to a TAFKAL80ETC concert", NormalExpiryStrategy.new, QualityStrategy.new(0, 50, {
       (0..4) => Delta.new(2),
-      (5..INF) => Delta.new(1),
-      (NEGINF..-1) => FixedValue.new(0)
+      (5..MAX_DATE) => Delta.new(1),
+      (MIN_DATE..-1) => FixedValue.new(0)
     })),
     SpecificItemProcessor.new("Sulfuras, Hand of Ragnaros", EternalExpiryStrategy.new, QualityStrategy.new(80, 80, {
-      (NEGINF..INF) => Delta.new(0),
+      (MIN_DATE..MAX_DATE) => Delta.new(0),
     }))
   ]
   FALLBACK_PROCESSOR = FallbackItemProcessor.new(NormalExpiryStrategy.new, QualityStrategy.new(0, 50, {
-    (0..INF) => Delta.new(-1),
-    (NEGINF..-1) => Delta.new(-2)
+    (0..MAX_DATE) => Delta.new(-1),
+    (MIN_DATE..-1) => Delta.new(-2)
   }))
 
   def initialize(items)
