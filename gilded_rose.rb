ecfe042a1b934d1d6bd4658fc5a55f7be200d9a5
Kay Lovelace <kay@herostrat.us>
2017-03-08 09:01:59 +0000

    Unroll ifs in backstage passes method
    Remains to be seen whether this will help

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -17,24 +17,22 @@ class GildedRose
   end
 
   def update_quality__backstage_passes(item)
-    if item.quality < 50
-      item.quality = item.quality + 1
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        if item.sell_in < 11
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-        if item.sell_in < 6
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
     item.sell_in = item.sell_in - 1
+
+    if item.quality < 50 && !((0..4).include?(item.sell_in))
+      item.quality += 1
+    end
+
+    if item.quality < 50 && (0..4).include?(item.sell_in)
+      item.quality += 1
+    end
+
+    if item.quality < 49 && (0..4).include?(item.sell_in)
+      item.quality += 2
+    end
+
     if item.sell_in < 0
-      item.quality = item.quality - item.quality
+      item.quality = 0
     end
   end
 
