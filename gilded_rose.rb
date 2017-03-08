4aec79f708c340c54931786d9dab6b4c58ad2a18
Kay Lovelace <kay@herostrat.us>
2017-03-08 09:04:10 +0000

    Remove the last of the nested ifs

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -9,10 +9,8 @@ class GildedRose
       item.quality = item.quality + 1
     end
 
-    if item.sell_in < 0
-      if item.quality < 50
-        item.quality = item.quality + 1
-      end
+    if item.sell_in < 0 && item.quality < 50
+      item.quality = item.quality + 1
     end
   end
 
@@ -43,10 +41,8 @@ class GildedRose
       item.quality = item.quality - 1
     end
 
-    if item.sell_in < 0
-      if item.quality > 0
-        item.quality = item.quality - 1
-      end
+    if item.sell_in < 0 &&  item.quality > 0
+      item.quality = item.quality - 1
     end
   end
 
