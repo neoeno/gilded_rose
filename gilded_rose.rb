9ff226c4ce53408d0dfbaaa46a300338dbe53e0a
Kay Lovelace <kay@makersacademy.com>
2017-03-08 14:53:30 +0000

    Add kwargs to QualityStrategy for readability

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -11,20 +11,20 @@ class GildedRose
   MAX_DATE = Float::INFINITY
   MIN_DATE = -(Float::INFINITY)
   ITEM_PROCESSORS = [
-    ItemProcessor::Specific.new("Aged Brie", ExpiryStrategy::Normal.new, QualityStrategy.new(0, 50, {
+    ItemProcessor::Specific.new("Aged Brie", ExpiryStrategy::Normal.new, QualityStrategy.new(min: 0, max: 50, ranges: {
       (MIN_DATE..-1) => QualityStrategy::Delta.new(2),
       (0..MAX_DATE) => QualityStrategy::Delta.new(1)
     })),
-    ItemProcessor::Specific.new("Backstage passes to a TAFKAL80ETC concert", ExpiryStrategy::Normal.new, QualityStrategy.new(0, 50, {
+    ItemProcessor::Specific.new("Backstage passes to a TAFKAL80ETC concert", ExpiryStrategy::Normal.new, QualityStrategy.new(min: 0, max: 50, ranges: {
       (0..4) => QualityStrategy::Delta.new(2),
       (5..MAX_DATE) => QualityStrategy::Delta.new(1),
       (MIN_DATE..-1) => QualityStrategy::FixedValue.new(0)
     })),
-    ItemProcessor::Specific.new("Sulfuras, Hand of Ragnaros", ExpiryStrategy::Eternal.new, QualityStrategy.new(80, 80, {
+    ItemProcessor::Specific.new("Sulfuras, Hand of Ragnaros", ExpiryStrategy::Eternal.new, QualityStrategy.new(min: 80, max: 80, ranges: {
       (MIN_DATE..MAX_DATE) => QualityStrategy::Delta.new(0),
     }))
   ]
-  FALLBACK_PROCESSOR = ItemProcessor::Fallback.new(ExpiryStrategy::Normal.new, QualityStrategy.new(0, 50, {
+  FALLBACK_PROCESSOR = ItemProcessor::Fallback.new(ExpiryStrategy::Normal.new, QualityStrategy.new(min: 0, max: 50, ranges: {
     (0..MAX_DATE) => QualityStrategy::Delta.new(-1),
     (MIN_DATE..-1) => QualityStrategy::Delta.new(-2)
   }))
--- lib/quality_strategy.rb
+++ lib/quality_strategy.rb
@@ -1,15 +1,15 @@
 class QualityStrategy
   Delta = Struct.new(:delta)
   FixedValue = Struct.new(:value)
-  
-  def initialize(min, max, ranges_to_changes)
+
+  def initialize(min:, max:, ranges:)
     @min = min
     @max = max
-    @ranges_to_changes = ranges_to_changes
+    @ranges = ranges
   end
 
   def advance(item)
-    _, change = ranges_to_changes.find { |range, _| range.include? item.sell_in }
+    _, change = ranges.find { |range, _| range.include? item.sell_in }
     if change.is_a? Delta
       new_quality = item.quality + change.delta
     elsif change.is_a? FixedValue
@@ -20,5 +20,5 @@ class QualityStrategy
 
   private
 
-  attr_reader :min, :max, :ranges_to_changes
+  attr_reader :min, :max, :ranges
 end
